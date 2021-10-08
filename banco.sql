1a6964b8d846c87cbb6647508dab5ce8f56b6faf
Tayllane <tbs4@aluno.ifal.edu.br>
2021-10-08 10:26:50 -0300

    first commit

---
+++ banco.sql
@@ -0,0 +1,11 @@
+ines (8 sloc)  312 Bytes
+   
+CREATE DATABASE `pweb`;
+
+CREATE TABLE  `pweb`.`USUARIO` (
+  `ID_USUARIO` int(10) unsigned NOT NULL auto_increment,
+  `NOME` varchar(200) NOT NULL default '',
+  `EMAIL` varchar(200) NOT NULL default '',
+  `SENHA` varchar(45) NOT NULL default '',
+  PRIMARY KEY  (`ID_USUARIO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
